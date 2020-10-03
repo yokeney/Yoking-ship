9fbc4c9cad97301fac385171a951d26a2dfabf7e
yokeney <965660471@qq.com>
2020-10-03 15:55:58 +0800

    button is half complate

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-
+import Button,{ButtonType,ButtonSize} from './components/button/button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button onClick={(e)=>{console.log(e)}} btnType={ButtonType.Default} size={ButtonSize.Small}>btn</Button>
+      <Button btnType={ButtonType.Link} size={ButtonSize.Small} href="http://www.baidu.com">baidu</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>btn</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>btn</Button>
     </div>
   );
 }
