3ae00ef420806051816a8e066e4de8e96ffa8626
yokeney <965660471@qq.com>
2020-10-18 23:13:19 +0800

    button is replaced

--- src/components/buton/button/button.tsx
+++ src/components/buton/button/button.tsx
@@ -10,7 +10,7 @@ export enum ButtonType {
     Danger = 'danger',
     Link = 'link',
 }
-interface BaseButtonProps {
+ interface BaseButtonProps {
     className?: string,
     disabled?: boolean,
     size?: ButtonSize,
@@ -18,39 +18,42 @@ interface BaseButtonProps {
     children: React.ReactNode
     href?: string
 }
-const Button: React.FC<BaseButtonProps> = (props) => {
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
     const {
+        className,
         disabled,
         size,
         btnType,
         children,
-        href
+        href,
+        ...restProps
     } = props
     const classes = classNames('btn', {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         "disabled": (btnType === ButtonType.Link) && disabled
     })
-    console.log(classes);
-
     if (btnType === ButtonType.Link && href) {
         return (
-            <a href={href} className={classes}>
+            <a href={href} className={classes} {...restProps}>
                 {children}
             </a>
         )
     }
-    else{
-        
+    else {
         return (
-            <button className={classes} disabled={disabled}>
+            <button className={classes} disabled={disabled} {...restProps}>
                 {children}
             </button>
         )
     }
 }
-Button.defaultProps={
-    disabled:false,
-    btnType:ButtonType.Default
+Button.defaultProps = {
+    disabled: false,
+    btnType: ButtonType.Default
 }
 export default Button
