23f61375902c1d64a5000202a555e602f354f5e8
yokeney <965660471@qq.com>
2020-10-18 23:33:26 +0800

    button is totle replaced

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render,fireEvent } from '@testing-library/react'
-import Button,{ButtonProps,ButtonType,ButtonSize} from './button'
+import Button,{ButtonProps,ButtonType,ButtonSize} from '../button/button'
 const defaultprops = {
     onClick:jest.fn()
 }
